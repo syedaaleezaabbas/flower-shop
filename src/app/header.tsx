b4fe9149eae9977ecf6f37a18c9087b3f2ed1184
syedaaleezaabbas <syedaaleezaabbas@gmail.com>
2024-11-25 19:25:04 +0500

    push flower shop to github

--- src/app/header.tsx
+++ src/app/header.tsx
@@ -1,33 +1,53 @@
 import Link from "next/link";
 import { FaCartShopping } from "react-icons/fa6";
 import { RxHamburgerMenu } from "react-icons/rx";
+import { Menu } from "lucide-react";
+import {
+    Sheet,
+    SheetContent,
+    SheetDescription,
+    SheetHeader,
+    SheetTitle,
+    SheetTrigger,
+} from "@/components/ui/sheet"
 
 export default function Header() {
     return (
-        <header className="bg-purple-300 shadow-md">
-            <nav className="container mx-auto p-4 flex justify-between items-center">
-                <div className="text-2xl font-bold">
-                    Florist Home Page
-                </div>
+        <header className="flex justify-between items-center max-w-screen-2xl mx-auto p-4 md:px-12 bg-purple-300 shadow-md">
+            <div className="md:text-2xl text-xl font-bold">
+                Florist Home Page
+            </div>
 
-                {/* Hamburger menu icon for smaller screens */}
-                <div className="md:hidden flex items-center pl-12">
-                    <button className="text-3xl">
-                        <RxHamburgerMenu />
-                    </button>
-                </div>
-
-                <ul className="flex items-center gap-x-6">
-                    <li className="hidden md:block"><Link href="/landing" className="text-black hover:text-gray-400">Landing</Link></li>
-                    <li className="hidden md:block"><Link href="/" className="text-black hover:text-gray-400">Home</Link></li>
-                    <li className="hidden md:block"><Link href="/gallery" className="text-black hover:text-gray-400">Gallery</Link></li>
-                    <li className="hidden md:block"><Link href="/shop" className="text-black hover:text-gray-400">Shop</Link></li>
-                    <li className="hidden md:block"><Link href="/About" className="text-black hover:text-gray-400">About</Link></li>
-                    <li className="hidden md:block"><Link href="/contact" className="text-black hover:text-gray-400">Contact Us</Link></li>
-                    <li className="hidden md:block"><Link href="/blog" className="text-black hover:text-gray-400">Blog</Link></li>
-                    <FaCartShopping className="text-2xl"/>
+            {/* desktop navbar */}
+            <nav className="hidden md:block">
+                <ul className="flex items-center text-lg gap-x-10">
+                    <li><Link href="/" className="text-black hover:text-gray-400">Home</Link></li>
+                    <li><Link href="/About" className="text-black hover:text-gray-400">About</Link></li>
+                    <li><Link href="/shop" className="text-black hover:text-gray-400">Shop</Link></li>
+                    <li><Link href="/shop" className="text-black hover:text-gray-400">Blog</Link></li>
+                    <li><Link href="/contact" className="text-black hover:text-gray-400">Contact Us</Link></li>
+                    <FaCartShopping className="text-xl" />
                 </ul>
             </nav>
+
+            {/* mobile navbar */}
+            <Sheet>
+                <SheetTrigger className="md:hidden flex justify-between items-center gap-x-4">
+                    <Menu className="text-xl" />
+                    <FaCartShopping className="text-xl" />
+                </SheetTrigger>
+                <SheetContent className="bg-gray-400 border-l-4 border-l-purple-500">
+                    <nav>
+                        <ul className="flex flex-col text-center pt-20 gap-y-4">
+                            <li><Link href="/" className="text-black hover:text-purple-500">Home</Link></li>
+                            <li><Link href="/About" className="text-black hover:text-purple-500">About</Link></li>
+                            <li><Link href="/shop" className="text-black hover:text-purple-500">Shop</Link></li>
+                            <li><Link href="/shop" className="text-black hover:text-purple-500">Blog</Link></li>
+                            <li><Link href="/contact" className="text-black hover:text-purple-500">Contact Us</Link></li>
+                        </ul>
+                    </nav>
+                </SheetContent>
+            </Sheet>
         </header>
     )
 }
--- src/app/specialities.tsx
+++ src/app/specialities.tsx
@@ -2,7 +2,7 @@ import Image from "next/image"
 
 export default function Specialities() {
     return (
-        <section className="py-20 bg-gray-50">
+        <section className="py-20 bg-gray-50 max-w-screen-2xl mx-auto">
             <div className="container mx-auto px-4 text-center">
                 <h2 className="text-4xl underline font-bold mb-6">Our Specialities</h2>
                 <p className="text-gray-900 max-w-2xl mx-auto mb-8">Nulla tempus sollicitudin dui, ut vehicula lacus forta vel. Duis urna ligula, luctus at feugiat a, lacinia ut sem. Quisque sit amet ipsum ac odio malesuada convallis. Lorem ipsum, dolor sit amet consectetur adipisicing elit. Reprehenderit non aut inventore laborum error fugiat dolores quo soluta minus esse, nostrum maiores et eligendi nulla exercitationem impedit quisquam hic dolorum?</p>
@@ -10,7 +10,12 @@ export default function Specialities() {
                 <div className="grid grid-cols-1 md:grid-cols-3 gap-12">
                     <div className="flex flex-col items-center">
                         <div className="w-[300px] h-[300px] md:w-[350px] md:h-[350px]">
-                            <Image src="/pic2.avif" alt="Rose" width={350} height={350} className="rounded-lg shadow-lg object-cover w-full h-full transition-transform duration-300 hover:scale-105 hover:shadow-2xl" /></div>
+                            <Image
+                                src="/pic2.avif"
+                                alt="Rose"
+                                width={350}
+                                height={350}
+                                className="rounded-lg shadow-lg object-cover w-full h-full transition-transform duration-300 hover:scale-105 hover:shadow-2xl" /></div>
                         <h3 className="text-2xl font-bold mt-2 mb-2">Roses</h3>
                         <p className="mb-3 md:mb-12">Beautiful, fresh roses for all occasions.</p>
                     </div>
@@ -18,7 +23,12 @@ export default function Specialities() {
 
                     <div className="flex flex-col items-center">
                         <div className="w-[300px] h-[300px] md:w-[350px] md:h-[350px]">
-                            <Image src="/pic1.avif" alt="tulip" width={350} height={350} className="rounded-lg shadow-lg object-cover w-full h-full transition-transform duration-300 hover:scale-105 hover:shadow-2xl" /></div>
+                            <Image
+                                src="/pic1.avif"
+                                alt="tulip"
+                                width={350}
+                                height={350}
+                                className="rounded-lg shadow-lg object-cover w-full h-full transition-transform duration-300 hover:scale-105 hover:shadow-2xl" /></div>
                         <h3 className="text-2xl font-bold mt-2 mb-2">Tulips</h3>
                         <p className="mb-3 md:mb-12">Vibrant tulips to brighten your day.</p>
                     </div>
@@ -26,7 +36,12 @@ export default function Specialities() {
 
                     <div className="flex flex-col items-center">
                         <div className="w-[300px] h-[300px] md:w-[350px] md:h-[350px]" >
-                            <Image src="/pic3.png" alt="lily" width={350} height={350} className="rounded-lg shadow-lg object-cover w-full h-full transition-transform duration-300 hover:scale-105 hover:shadow-2xl" /></div>
+                            <Image
+                                src="/pic3.png"
+                                alt="lily"
+                                width={350}
+                                height={350}
+                                className="rounded-lg shadow-lg object-cover w-full h-full transition-transform duration-300 hover:scale-105 hover:shadow-2xl" /></div>
                         <h3 className="text-2xl font-bold mt-2 mb-2">Lilies</h3>
                         <p className="mb-16 md:mb-12">Elegant lilies to bring peace and beauty.</p>
                     </div>
@@ -36,7 +51,12 @@ export default function Specialities() {
                 <div className="grid grid-cols-1 md:grid-cols-3 gap-12">
                     <div className="flex flex-col items-center">
                         <div className="w-[300px] h-[300px] md:w-[350px] md:h-[350px]">
-                            <Image src="/Sunflowers.webp" alt="sunflowers" width={350} height={350} className="rounded-lg shadow-lg object-cover w-full h-full transition-transform duration-300 hover:scale-105 hover:shadow-2xl" /></div>
+                            <Image
+                                src="/Sunflowers.webp"
+                                alt="sunflowers"
+                                width={350}
+                                height={350}
+                                className="rounded-lg shadow-lg object-cover w-full h-full transition-transform duration-300 hover:scale-105 hover:shadow-2xl" /></div>
                         <h3 className="text-2xl font-bold mt-2 mb-2">Sunflowers</h3>
                         <p className="mb-3 md:mb-0">Bright and cheerful sunflowers to light up any space.</p>
                     </div>
@@ -44,7 +64,12 @@ export default function Specialities() {
 
                     <div className="flex flex-col items-center">
                         <div className="w-[300px] h-[300px] md:w-[350px] md:h-[350px]">
-                            <Image src="/daisies.jpg" alt="daisies" width={350} height={350} className="rounded-lg shadow-lg object-cover w-full h-full transition-transform duration-300 hover:scale-105 hover:shadow-2xl" /></div>
+                            <Image
+                                src="/daisies.jpg"
+                                alt="daisies"
+                                width={350}
+                                height={350}
+                                className="rounded-lg shadow-lg object-cover w-full h-full transition-transform duration-300 hover:scale-105 hover:shadow-2xl" /></div>
                         <h3 className="text-2xl font-bold mt-2 mb-2">Daisies</h3>
                         <p className="mb-3 md:mb-0">Fresh Daisies symbolize purity and simplicity.</p>
                     </div>
@@ -52,7 +77,12 @@ export default function Specialities() {
 
                     <div className="flex flex-col items-center">
                         <div className="w-[300px] h-[300px] md:w-[350px] md:h-[350px]">
-                            <Image src="/orchids.avif" alt="orchids" width={350} height={350} className="rounded-lg shadow-lg object-cover w-full h-full transition-transform duration-300 hover:scale-105 hover:shadow-2xl" /></div>
+                            <Image
+                                src="/orchids.avif"
+                                alt="orchids"
+                                width={350}
+                                height={350}
+                                className="rounded-lg shadow-lg object-cover w-full h-full transition-transform duration-300 hover:scale-105 hover:shadow-2xl" /></div>
                         <h3 className="text-2xl font-bold mt-2 mb-2">Orchids</h3>
                         <p className="mb-4 md:mb-0">Exotic orchids that add elegance and sophistication.</p>
                     </div>
@@ -62,7 +92,11 @@ export default function Specialities() {
                 <div className="grid grid-cols-1 md:grid-cols-3 gap-12 mt-12">
                     <div className="flex flex-col items-center">
                         <div className="w-[300px] h-[300px] md:w-[350px] md:h-[350px]">
-                            <Image src="/hydrangea.png" alt="hydrangea" width={350} height={350} className="rounded-lg shadow-lg object-cover w-full h-full transition-transform duration-300 hover:scale-105 hover:shadow-2xl" /></div>
+                            <Image src="/hydrangea.png"
+                                alt="hydrangea"
+                                width={350}
+                                height={350}
+                                className="rounded-lg shadow-lg object-cover w-full h-full transition-transform duration-300 hover:scale-105 hover:shadow-2xl" /></div>
                         <h3 className="text-2xl font-bold mt-2 mb-2">Hydrangea</h3>
                         <p className="mb-4 md:mb-0">Hydrangeas represent gratitude and grace.</p>
                     </div>
@@ -70,7 +104,12 @@ export default function Specialities() {
 
                     <div className="flex flex-col items-center">
                         <div className="w-[300px] h-[300px] md:w-[350px] md:h-[350px]">
-                            <Image src="/iris.avif" alt="iris" width={350} height={350} className="rounded-lg shadow-lg object-cover w-full h-full transition-transform duration-300 hover:scale-105 hover:shadow-2xl" /></div>
+                            <Image
+                                src="/iris.avif"
+                                alt="iris"
+                                width={350}
+                                height={350}
+                                className="rounded-lg shadow-lg object-cover w-full h-full transition-transform duration-300 hover:scale-105 hover:shadow-2xl" /></div>
                         <h3 className="text-2xl font-bold mt-2 mb-2">Iris</h3>
                         <p className="mb-4 md:mb-0">The iris with its striking purple petals, symbolizes elegance and serenity.</p>
                     </div>
@@ -78,7 +117,12 @@ export default function Specialities() {
 
                     <div className="flex flex-col items-center">
                         <div className="w-[300px] h-[300px] md:w-[350px] md:h-[350px]">
-                            <Image src="/daffodils.webp" alt="daffodils" width={350} height={350} className="rounded-lg shadow-lg object-cover w-full h-full transition-transform duration-300 hover:scale-105 hover:shadow-2xl" /></div>
+                            <Image
+                                src="/daffodils.webp"
+                                alt="daffodils"
+                                width={350}
+                                height={350}
+                                className="rounded-lg shadow-lg object-cover w-full h-full transition-transform duration-300 hover:scale-105 hover:shadow-2xl" /></div>
                         <h3 className="text-2xl font-bold mt-2 mb-2">Daffodils</h3>
                         <p className="mb-4 md:mb-0">Daffodils symbolize renewel and joy with their bright yellow blooms.</p>
                     </div>
@@ -88,7 +132,12 @@ export default function Specialities() {
                 <div className="grid grid-cols-1 md:grid-cols-3 gap-12 mt-12">
                     <div className="flex flex-col items-center">
                         <div className="w-[300px] h-[300px] md:w-[350px] md:h-[350px]">
-                            <Image src="/jasmine.avif" alt="jasmine" width={350} height={350} className="rounded-lg shadow-lg object-cover w-full h-full transition-transform duration-300 hover:scale-105 hover:shadow-2xl" /></div>
+                            <Image
+                                src="/jasmine.avif"
+                                alt="jasmine"
+                                width={350}
+                                height={350}
+                                className="rounded-lg shadow-lg object-cover w-full h-full transition-transform duration-300 hover:scale-105 hover:shadow-2xl" /></div>
                         <h3 className="text-2xl font-bold mt-2 mb-2">Jasmine</h3>
                         <p className="mb-4 md:mb-0">Jasmine emits a sweet frangrance.</p>
                     </div>
@@ -96,7 +145,12 @@ export default function Specialities() {
 
                     <div className="flex flex-col items-center">
                         <div className="w-[300px] h-[300px] md:w-[350px] md:h-[350px]">
-                            <Image src="/lavendar.jpg" alt="lavendar" width={350} height={350} className="rounded-lg shadow-lg object-cover w-full h-full transition-transform duration-300 hover:scale-105 hover:shadow-2xl" /></div>
+                            <Image
+                                src="/lavendar.jpg"
+                                alt="lavendar"
+                                width={350}
+                                height={350}
+                                className="rounded-lg shadow-lg object-cover w-full h-full transition-transform duration-300 hover:scale-105 hover:shadow-2xl" /></div>
                         <h3 className="text-2xl font-bold mt-2 mb-2">Lavendar</h3>
                         <p className="mb-4 md:mb-0">Lavender soothes and calms the mind.</p>
                     </div>
@@ -104,7 +158,12 @@ export default function Specialities() {
 
                     <div className="flex flex-col items-center">
                         <div className="w-[300px] h-[300px] md:w-[350px] md:h-[350px]">
-                            <Image src="/marigold.jpg" alt="marigold" width={350} height={350} className="rounded-lg shadow-lg object-cover w-full h-full transition-transform duration-300 hover:scale-105 hover:shadow-2xl" /></div>
+                            <Image
+                                src="/marigold.jpg"
+                                alt="marigold"
+                                width={350}
+                                height={350}
+                                className="rounded-lg shadow-lg object-cover w-full h-full transition-transform duration-300 hover:scale-105 hover:shadow-2xl" /></div>
                         <h3 className="text-2xl font-bold mt-2 mb-2">Marigolds</h3>
                         <p className="mb-4 md:mb-0">Marigolds add vibrant color to any garden.</p>
                     </div>
